9d7ec2fe1f8ef12a0ad071985f00b0a785171ecf
suflors <iammagical.space@gmail.com>
2023-12-16 12:59:44 -0800

    turned program.cs into a class

--- AtelierTomato.Soil/AtelierTomato.Soil/Program.cs
+++ AtelierTomato.Soil/AtelierTomato.Soil/Program.cs
@@ -1,6 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
-
-Console.WriteLine("Welcome to my cool program!");
-Console.WriteLine("In this program we will be soiling.");
-Console.WriteLine("Goodbye, World!");
+﻿namespace AtelierTomato.Soil
+{
+	public class Program
+	{
+		public static int Main()
+		{
+			Console.WriteLine("Welcome to my cool program!");
+			Console.WriteLine("In this program we will be soiling.");
+			Console.WriteLine("Goodbye, World!");
+			return 0;
+		}
+	}
+}
