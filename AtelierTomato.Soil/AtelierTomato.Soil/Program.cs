2b3dd235eaa1a94678dfefbeb81fbffd96750c14
suflors <iammagical.space@gmail.com>
2023-12-16 12:47:29 -0800

    causing a merge conflict

--- AtelierTomato.Soil/AtelierTomato.Soil/Program.cs
+++ AtelierTomato.Soil/AtelierTomato.Soil/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to my cool program!");
+Console.WriteLine("In this program we will be soiling.");
