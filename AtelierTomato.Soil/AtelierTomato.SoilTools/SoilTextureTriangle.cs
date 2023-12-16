abec523918a0f5a31af2ac7d47d01cc71e626aa8
suflors <iammagical.space@gmail.com>
2023-12-16 14:10:55 -0800

    added a dictionary to SoilTextureTriangle which includes the regions mapped to specific types of soil

--- AtelierTomato.Soil/AtelierTomato.SoilTools/SoilTextureTriangle.cs
+++ AtelierTomato.Soil/AtelierTomato.SoilTools/SoilTextureTriangle.cs
@@ -12,5 +12,18 @@ namespace AtelierTomato.SoilTools
 			// Calculate percentage of each mineral type and make it to a Vector3
 			point = new Vector3(clay / total * 100, silt / total * 100, sand / total * 100);
 		}
+
+		public static Vector3[][] ClayRegion = new[] {
+				new [] { new Vector3 ( 100.0f,   0.0f,   0.0f ), new Vector3 (  60.0f,  40.0f,   0.0f ), new Vector3 (  55.0f,   0.0f,  45.0f ) },
+				new [] { new Vector3 (  60.0f,  40.0f,   0.0f ), new Vector3 (  40.0f,  15.0f,  45.0f ), new Vector3 (  55.0f,   0.0f,  45.0f ) },
+				new [] { new Vector3 (  60.0f,  40.0f,   0.0f ), new Vector3 (  40.0f,  40.0f,  20.0f ), new Vector3 (  40.0f,  15.0f,  20.0f ) },
+		};
+		public static Vector3[][] SiltyClayRegion = new[] {
+				new [] { new Vector3 (  60.0f,  40.0f,   0.0f ), new Vector3 (  40.0f,  60.0f,   0.0f ), new Vector3 (  40.0f,  40.0f,  20.0f ) }
+		};
+		public static Dictionary<string, Vector3[][]> AllSections = new() {
+			{ "Clay", ClayRegion },
+			{ "Silty Clay", SiltyClayRegion }
+		};
 	}
 }
