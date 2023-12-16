11146ebb7c1a26b20aac98ca38ab2d0aa9c90459
suflors <iammagical.space@gmail.com>
2023-12-16 14:02:49 -0800

    created a basic constructor for SoilTextureTriangle

--- AtelierTomato.Soil/AtelierTomato.SoilTools/SoilTextureTriangle.cs
+++ AtelierTomato.Soil/AtelierTomato.SoilTools/SoilTextureTriangle.cs
@@ -1,7 +1,16 @@
-﻿namespace AtelierTomato.SoilTools
-{
-	public class SoilTextureTriangle
-	{
+﻿using System.Numerics;
 
+namespace AtelierTomato.SoilTools
+{
+	public record SoilTextureTriangle
+	{
+		private readonly Vector3 point;
+
+		public SoilTextureTriangle(float clay, float silt, float sand)
+		{
+			var total = clay + silt + sand;
+			// Calculate percentage of each mineral type and make it to a Vector3
+			point = new Vector3(clay / total * 100, silt / total * 100, sand / total * 100);
+		}
 	}
 }
