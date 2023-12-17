dd4bc54ce34f9772a09d6fa7469bd65d41273198
suflors <iammagical.space@gmail.com>
2023-12-16 17:09:15 -0800

    updated the order of the soil textures in the dictionary to change which soil texture is determined

--- AtelierTomato.Soil/AtelierTomato.SoilTools/SoilTexture.cs
+++ AtelierTomato.Soil/AtelierTomato.SoilTools/SoilTexture.cs
@@ -30,17 +30,6 @@ namespace AtelierTomato.SoilTools
 		}
 
 		// Construct a soil texture triangle split into regions that are assigned specific soil textures as a dictionary
-		private static Vector3[][] ClayRegion = new[] {
-			new [] { new Vector3 ( 100.0f,   0.0f,   0.0f ), new Vector3 (  60.0f,  40.0f,   0.0f ), new Vector3 (  55.0f,   0.0f,  45.0f ) },
-			new [] { new Vector3 (  60.0f,  40.0f,   0.0f ), new Vector3 (  40.0f,  15.0f,  45.0f ), new Vector3 (  55.0f,   0.0f,  45.0f ) },
-			new [] { new Vector3 (  60.0f,  40.0f,   0.0f ), new Vector3 (  40.0f,  40.0f,  20.0f ), new Vector3 (  40.0f,  15.0f,  20.0f ) }
-		};
-		private static Vector3[][] SiltyClayRegion = new[] {
-			new [] { new Vector3 (  60.0f,  40.0f,   0.0f ), new Vector3 (  40.0f,  60.0f,   0.0f ), new Vector3 (  40.0f,  40.0f,  20.0f ) }
-		};
-		private static Vector3[][] SandyClayRegion = new[] {
-			new [] { new Vector3 (  55.0f,   0.0f,  45.0f ), new Vector3 (  35.0f,  20.0f,  45.0f ), new Vector3 (  35.0f,   0.0f,  65.0f ) }
-		};
 		private static Vector3[][] ClayLoamRegion = new[] {
 			new [] { new Vector3 (  40.0f,  15.0f,  45.0f ), new Vector3 (  40.0f,  40.0f,  20.0f ), new Vector3 (  27.5f,  27.5f,  45.0f ) },
 			new [] { new Vector3 (  40.0f,  40.0f,  20.0f ), new Vector3 (  27.5f,  27.5f,  45.0f ), new Vector3 (  27.5f,  52.5f,  20.0f ) }
@@ -72,31 +61,42 @@ namespace AtelierTomato.SoilTools
 			new [] { new Vector3 (   7.5f,  40.0f,  52.5f ), new Vector3 (   7.5f,  50.0f,  42.5f ), new Vector3 (   0.0f,  50.0f,  50.0f ) },
 			new [] { new Vector3 (   7.5f,  40.0f,  52.5f ), new Vector3 (   0.0f,  50.0f,  50.0f ), new Vector3 (   0.0f,  30.0f,  70.0f ) }
 		};
-		private static Vector3[][] SiltRegion = new[] {
-			new [] { new Vector3 (  12.5f,  80.0f,   7.5f ), new Vector3 (  12.5f,  87.5f,   0.0f ), new Vector3 (   0.0f, 100.0f,   0.0f ) },
-			new [] { new Vector3 (  12.5f,  80.0f,   7.5f ), new Vector3 (   0.0f, 100.0f,   0.0f ), new Vector3 (   0.0f,  80.0f,  20.0f ) }
+		private static Vector3[][] SiltyClayRegion = new[] {
+			new [] { new Vector3 (  60.0f,  40.0f,   0.0f ), new Vector3 (  40.0f,  60.0f,   0.0f ), new Vector3 (  40.0f,  40.0f,  20.0f ) }
+		};
+		private static Vector3[][] SandyClayRegion = new[] {
+			new [] { new Vector3 (  55.0f,   0.0f,  45.0f ), new Vector3 (  35.0f,  20.0f,  45.0f ), new Vector3 (  35.0f,   0.0f,  65.0f ) }
 		};
 		private static Vector3[][] LoamySandRegion = new[] {
 			new [] { new Vector3 (  15.0f,   0.0f,  85.0f ), new Vector3 (   0.0f,  30.0f,  70.0f ), new Vector3 (  10.0f,   0.0f,  90.0f ) },
 			new [] { new Vector3 (  10.0f,   0.0f,  90.0f ), new Vector3 (   0.0f,  30.0f,  70.0f ), new Vector3 (   0.0f,  15.0f,  85.0f ) }
 		};
+		private static Vector3[][] SiltRegion = new[] {
+			new [] { new Vector3 (  12.5f,  80.0f,   7.5f ), new Vector3 (  12.5f,  87.5f,   0.0f ), new Vector3 (   0.0f, 100.0f,   0.0f ) },
+			new [] { new Vector3 (  12.5f,  80.0f,   7.5f ), new Vector3 (   0.0f, 100.0f,   0.0f ), new Vector3 (   0.0f,  80.0f,  20.0f ) }
+		};
 		private static Vector3[][] SandRegion = new[] {
 			new [] { new Vector3 (  10.0f,   0.0f,  90.0f ), new Vector3 (   0.0f,  15.0f,  85.0f ), new Vector3 (   0.0f,   0.0f, 100.0f ) }
 		};
+		private static Vector3[][] ClayRegion = new[] {
+			new [] { new Vector3 ( 100.0f,   0.0f,   0.0f ), new Vector3 (  60.0f,  40.0f,   0.0f ), new Vector3 (  55.0f,   0.0f,  45.0f ) },
+			new [] { new Vector3 (  60.0f,  40.0f,   0.0f ), new Vector3 (  40.0f,  15.0f,  45.0f ), new Vector3 (  55.0f,   0.0f,  45.0f ) },
+			new [] { new Vector3 (  60.0f,  40.0f,   0.0f ), new Vector3 (  40.0f,  40.0f,  20.0f ), new Vector3 (  40.0f,  15.0f,  20.0f ) }
+		};
 
 		public static Dictionary<string, Vector3[][]> SoilTextureTriangle = new() {
-			{ "Clay", ClayRegion },
-			{ "Silty Clay", SiltyClayRegion },
-			{ "Sandy Clay", SandyClayRegion },
 			{ "Clay Loam", ClayLoamRegion },
 			{ "Silty Clay Loam", SiltyClayLoamRegion },
 			{ "Sandy Clay Loam", SandyClayLoamRegion },
 			{ "Loam", LoamRegion },
 			{ "Silt Loam", SiltLoamRegion },
 			{ "Sandy Loam", SandyLoamRegion },
-			{ "Silt", SiltRegion },
+			{ "Silty Clay", SiltyClayRegion },
+			{ "Sandy Clay", SandyClayRegion },
 			{ "Loamy Sand", LoamySandRegion },
-			{ "Sand", SandRegion }
+			{ "Silt", SiltRegion },
+			{ "Sand", SandRegion },
+			{ "Clay", ClayRegion }
 		};
 
 		private string DetermineSoilTexture() => DetermineSoilClassifications().First();
