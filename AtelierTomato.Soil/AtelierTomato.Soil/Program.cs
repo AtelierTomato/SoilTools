bc26be4de70d3b26b9a5266e7dd4e8b1add3aed8
suflors <iammagical.space@gmail.com>
2023-12-16 17:34:21 -0800

    updated Program.cs's user interfacing

--- AtelierTomato.Soil/AtelierTomato.Soil/Program.cs
+++ AtelierTomato.Soil/AtelierTomato.Soil/Program.cs
@@ -6,15 +6,24 @@ namespace AtelierTomato.Soil
 	{
 		public static void Main()
 		{
-			Console.WriteLine("Welcome to my cool program!");
-			Console.WriteLine("In this program we will be soiling.");
-			SoilTexture myGround = new SoilTexture(80, 20, 10);
-			SoilTexture yourGround = new SoilTexture(40, 20, 40);
-			Console.WriteLine("Please input three numeric values assigned to clay, silt, and sand, respectively, seperated by spaces:");
-			var userInput = Console.ReadLine();
-			var userSoilTextureValues = userInput.Split(' ').Select(s => float.Parse(s)).ToArray();
-			SoilTexture userGround = new SoilTexture(userSoilTextureValues[0], userSoilTextureValues[1], userSoilTextureValues[2]);
-			Console.WriteLine(userGround.Name);
+			Console.WriteLine("This program will allow you to enter the mineral make up of your soil and calculate the soil texture.");
+			Console.WriteLine();
+			bool loop = true;
+			while (loop)
+			{
+				Console.WriteLine("Please input three numeric values assigned to clay, silt, and sand, respectively, seperated by spaces:");
+				var userInput = Console.ReadLine();
+				var userSoilTextureValues = userInput.Split(' ').Select(s => float.Parse(s)).ToArray();
+				SoilTexture userGround = new SoilTexture(userSoilTextureValues[0], userSoilTextureValues[1], userSoilTextureValues[2]);
+				Console.WriteLine(userGround.Name);
+				Console.WriteLine();
+				Console.WriteLine("Press 'X' to close the program or any other key to calculate another soil texture.");
+				if (Console.ReadKey(false).Key == ConsoleKey.X)
+				{
+					loop = false;
+				}
+				Console.WriteLine();
+			}
 		}
 	}
 }
